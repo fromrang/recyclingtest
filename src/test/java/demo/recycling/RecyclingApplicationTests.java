44a82036cc39309445e32db1cfd5cac48b1f08ab
test <test@test.com>
2022-11-01 01:04:43 +0900

    test

---
+++ src/test/java/demo/recycling/RecyclingApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.recycling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyclingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
