d316b58094b089f1228e1020543d28844b1c1ecf
test <test@test.com>
2022-11-17 10:22:21 +0900

    [MOD] social login, room insert, select

--- src/main/java/demo/recycling/RecyclingApplication.java
+++ src/main/java/demo/recycling/RecyclingApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class RecyclingApplication extends SpringBootServletInitializer {
@@ -11,6 +13,14 @@ public class RecyclingApplication extends SpringBootServletInitializer {
 //	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 //		return builder.sources(RecyclingApplication.class);
 //	}
+
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecyclingApplication.class, args);
