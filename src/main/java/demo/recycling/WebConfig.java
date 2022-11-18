dd1fa83edeae3fc8e3c0afb965613ef01a937568
test <test@test.com>
2022-11-18 11:31:54 +0900

    [MOD] cors 수정

--- src/main/java/demo/recycling/WebConfig.java
+++ src/main/java/demo/recycling/WebConfig.java
@@ -9,7 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000");
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://119.209.77.170:9926/");
     }
 }
