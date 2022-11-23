e6429701c8bafb2e96a85a4ce4553c6f7e16b7a8
rhksgud123 <rhksgud123@naver.com>
2022-11-23 11:51:32 +0900

    이메일 수정

--- src/main/java/demo/recycling/controller/MailController.java
+++ src/main/java/demo/recycling/controller/MailController.java
@@ -3,7 +3,9 @@ package demo.recycling.controller;
 import demo.recycling.dto.DefaultRes;
 import demo.recycling.dto.Mail;
 import demo.recycling.dto.StatusCode;
+import demo.recycling.repository.SuggestionDao;
 import demo.recycling.service.MailService;
+import demo.recycling.service.SuggestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,9 @@ public class MailController {
     @Autowired
     MailService mailService;
 
+    @Autowired
+    SuggestionService suggestionService;
+
     @PostMapping("/mail")
     public ResponseEntity sendMail(@RequestBody Mail mail) {
         Boolean result = mailService.insertMail(mail);
@@ -22,6 +27,7 @@ public class MailController {
             return new ResponseEntity(DefaultRes.res(StatusCode.BAD_REQUEST, "[FAIL]mailInsert"), HttpStatus.BAD_REQUEST);
         } else {
             mailService.mailSend(mail);
+            suggestionService.serviceSuggestionDelete(mail.getSseq());
             return new ResponseEntity(DefaultRes.res(StatusCode.OK, "[SUCCESS]mailInsert", mail), HttpStatus.OK);
         }
     }
