f33fff2da02e7ebcd89da998bb992f76f01d3658
rhksgud123 <rhksgud123@naver.com>
2022-11-23 11:31:15 +0900

    방 입장 수정 방 입장(정원수,같은 닉네임)과 방 인원 추가 분리

--- src/main/java/demo/recycling/dto/StatusCode.java
+++ src/main/java/demo/recycling/dto/StatusCode.java
@@ -10,6 +10,9 @@ public class StatusCode {
     public static final int NO_CONTENT = 204;
 
     public static final int COUNT_OVER = 205;
+
+    public static final int COUNT_OK = 206;
+
     public static final int BAD_REQUEST =  400;
     public static final int UNAUTHORIZED = 401;
     public static final int FORBIDDEN = 403;
