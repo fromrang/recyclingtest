7549a4577d1933c63cc448c9f45df34b5b49baea
test <test@test.com>
2022-12-12 18:25:36 +0900

    [MOD] searchMapper DB name change

--- src/main/java/demo/recycling/mapper/SearchMapper.java
+++ src/main/java/demo/recycling/mapper/SearchMapper.java
@@ -9,10 +9,10 @@ import java.util.List;
 @Mapper
 public interface SearchMapper {
 
-    @Select("SELECT * FROM recycling.searchDetail where d_no=#{d_no)}")
+    @Select("SELECT * FROM searchDetail where d_no=#{d_no)}")
     public SearchDetail selectDetailSearch(int d_no) throws Exception;
 
-    @Select("SELECT s_no FROM recycling.searchResult where r_name like CONCAT('%', #{keyword}, '%')")
+    @Select("SELECT s_no FROM searchResult where r_name like CONCAT('%', #{keyword}, '%')")
     public List<String> selectResultsearch(String keyword) throws Exception;
 
 }
